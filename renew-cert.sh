ce15c3a3a0b195f93270a668dd00a7405b5acd40
Satyanarayan Ray Pitambar Mohapatra <satyanarayan.raypitambarmohapatra@ligo.org>
2023-04-12 20:25:22 +0000

    Update renew-cert.sh

--- renew-cert.sh
+++ renew-cert.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/usr/bin/bash
 # Renew the robot certificate from the Kerberos keytab.
 set -e
+
+# This script is designed to be run by cron.
+# Set up the same environemnt variables as for a login shell.
+source "$HOME/.bashrc"
+
 X509_USER_CERT="$HOME/.globus/usercert.pem"
 X509_USER_KEY="$HOME/.globus/userkey.pem"
 KERBEROS_KEYTAB="${HOME}/.globus/krb5.keytab"
 KERBEROS_PRINCIPAL="$(klist -k "${KERBEROS_KEYTAB}" | tail -n 1 | sed 's/.*\s//')"
 kinit "${KERBEROS_PRINCIPAL}" -k -t "${KERBEROS_KEYTAB}"
-ligo-proxy-init -k > /dev/null
-GRID_PROXY_PATH="$(grid-proxy-info -path)"
+ecp-get-cert -k > /dev/null
+GRID_PROXY_PATH="$(ecp-cert-info -path)"
 cp "${GRID_PROXY_PATH}" "${X509_USER_CERT}"
 cp "${GRID_PROXY_PATH}" "${X509_USER_KEY}"
