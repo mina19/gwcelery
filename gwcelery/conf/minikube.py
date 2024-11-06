677b2a968370f39b2cc0caadb1c4fbe608fad4ab
Roberto De Pietri <roberto.depietri@unipr.it>
2024-11-06 04:42:54 -0800

    Allow the use of minikube configuration in fluxuser

--- gwcelery/conf/minikube.py
+++ gwcelery/conf/minikube.py
@@ -1,12 +1,20 @@
 """Application configuration for ``minikube`` local installation."""
 
+import os
+
 from . import *  # noqa: F401, F403
 
 expose_to_public = True
 """Set to True if events meeting the public alert threshold really should be
 exposed to the public."""
 
-igwn_alert_server = 'kafka://hopskotch-server'
+gracedb_host = os.getenv('GRACEDB_HOSTNAME',
+                         'gracedb.default.svc.cluster.local')
+"""GraceDB host."""
+
+
+igwn_alert_server = os.getenv('IGWN_HOSTNAME',
+                              'kafka://hopskotch-server')
 """IGWN alert server: None == DEFAULT_SERVER"""
 
 igwn_alert_noauth = True
@@ -14,9 +22,6 @@ igwn_alert_noauth = True
 
 igwn_alert_group = 'default'
 """IGWN alert group."""
-
-gracedb_host = 'gracedb.default.svc.cluster.local'
-"""GraceDB host."""
 
 mock_events_simulate_multiple_uploads = False
 """If True, then upload each mock event several times in rapid succession with
@@ -28,8 +33,10 @@ kafka_consumer_config = {
 messages to be consumed. The values are a dictionary of the URL to listen to
 and information about the message serializer."""
 
+kafka_alert_server = os.getenv('KAFKA_HOSTNAME',
+                               'kafka://hopskotch-server')
 kafka_alert_config = {
-    'scimma': {'url': 'kafka://hopskotch-server/igwn.gwalert-minikube',
+    'scimma': {'url': kafka_alert_server + '/igwn.gwalert-minikube',
                'suffix': 'avro', 'skymap_encoder': lambda _: _,
                'auth': False}
 }
